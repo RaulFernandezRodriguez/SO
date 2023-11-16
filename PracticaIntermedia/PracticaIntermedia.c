9040ca97c0932c26271c829c655764bde92fce2a
Raul <rfernr08@estudiantes.unileon.es>
2023-11-16 19:11:23 +0100

    hacer metodo calcular aleatorio

--- PracticaIntermedia/PracticaIntermedia.c
+++ PracticaIntermedia/PracticaIntermedia.c
@@ -12,7 +12,7 @@ void asistenteHandler(int i);
 
 int main(int argc, char*argv[]){ //coordinador es  padre
     struct sigaction ss;
-    srand (time(NULL));
+    srand (time(NULL)); // getpid
     pid_t tecnico, encargado, a;
     tecnico = fork();
     if(tecnico == 0){
@@ -24,13 +24,13 @@ int main(int argc, char*argv[]){ //coordinador es  padre
         ss.sa_handler = &encargadoHandler;  
         pause();
     }
-    int asistentes[argc];
+    asistentes = (pid_t *)malloc(sizeof(pid_t) * argc);
     for (int i=0; i < argc; i++) {
         a = fork();
         if (a == -1) {
             perror("Error en la llamada a fork()");
         } else if (a == 0) {
-            asistentes[i] = a;
+            *(asistentes + i) = a;
             pause();
         }
     }
@@ -41,37 +41,53 @@ int main(int argc, char*argv[]){ //coordinador es  padre
     if(vuelo == 0){
         kill(tecnico, SIGTERM);
         kill(encargado, SIGTERM);
-        for(int i = 0; i < asistentes.length(); i++){
-            kill(asistente[i], SIGTERM);
+        for(int i = 0; i < sizeof(asistentes); i++){
+            kill(*(asistentes +i), SIGTERM);
         }
     }
 
-    int pasajeros = 0;
+
     int overbooking = sigaction(SIGUSR1, &ss, NULL);
-    pause();
-    for(int i = 0; i < asistentes.length(); i++){
-        pasajeros =  pasajeros + sigaction(SIGUSR2, &ss, NULL);//signal(SIGUSR2, asistentes[i]);
+    pause(); 
+    if(coordinador){
+        int pasajeros = 0;
+    for(int i = 0; i < sizeof(asistentes); i++){
+        kill(*(asistentes), SIGUSR2);
+    }
+    while(pid != -1){
+        pid = wait(&status);
+        if(WIFSIGNALED(wstatus)){
+            
+        }
+        pasajeros = pasajeros + WEXITSTATUS(status);
+        
     }
     if(overbooking == 1){
+        printf("Buf,se nos ha ido de manos el planteamiento y hemos tenido overbooking");
         pasajeros = pasajeros -10;
+    }printf("Los %d pasajeros estan preparados para el vuelo, disfruten del viaje.\n", pasajeros);
     }
-    printf(pasajeros);
+    
+    free(asistentes);
+    
 }
 
     void tecnicoHandler(int i){
-        printf("Tecnico contactado");
+        printf("Tecnico contactado.\n");
         sleep(rand() %4 + 3);
         exit(rand() %2 + 0);
     }
 
     void encargadoHandler(int i){
-        printf("Encargado contactado");
+        printf("Encargado contactado.\n");
         sleep(2);
         exit(rand() %2 + 0);
     }
 
     void asistenteHandler(int i){
-        printf("Asistente" + getpid() + "contactado");
+        printf("Asistente %d contactado.\n", getpid());
         sleep(rand() %4 + 3);
-        exit(rand() %11 + 20);
+        int pasajeros = rand() % 11 +20;
+        printf("El asistente %d hay conseguido embarcar a unos %d pasajeros.\n", getpid(), pasajeros);
+        exit(pasajeros);
     }
--- PracticaIntermedia/script.sh
+++ PracticaIntermedia/script.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
-    while true; do  
-        menu
-        read opcion
-        case $option in
-        1)
-            cat *.c
-            ;;
-        2)
-            gcc *.c -o vuelo.intento
-            echo "Vuelo compilado"
-            ;;
-        3)  
-            existeEjecutable
-            if test [$existen = true] -a [-x vuelo.intento]; then 
-                ./vuelo.intento $assistants           
-            else
-                echo "Fallo al intentar ejecutar el vuelo, comprueba y vuelve a intentar"
-            fi
-            ;;
-        4)
-            echo "Ya has tenido suficientes vuelos, saliendo"
-            exit0
-            ;;
-        *) 
-            echo "Esta opcion no existe, vuelve a intentar con una opcion valida"
-            ;;
-        esac
-    done
 
 function menu(){
     echo "Que quieres hacer:"
@@ -38,11 +10,9 @@ function menu(){
 
 function get_passengers(){
     incorrecto = "^[0-9]+$"
-
     while true; do
         echo "Para cuantos es el vuelo?"
         read assistants
-
         if [$assistants = $incorrecto]; then
             echo "Pero si eso no es un numero, vuelve a intentarlo"
         elif test assistants -lt 0; then
@@ -51,14 +21,36 @@ function get_passengers(){
             echo "Todo correcto, preparando el vuelo..."
             break
         fi
-    fi
     done
 }
 
-function existeEjecutable(){
-    existen = false;
-    for i in *; do
-        if [$i = vuelo.prueba]; then 
-            $existen = true
+    while true; do  
+        menu
+        read opcion
+        case $opcion in
+        1)
+            cat PracticaIntermedia.c
+            ;;
+        2)
+            gcc PracticaIntermedia.c -o vuelo.intento
+            echo "Vuelo compilado"
+            ;;
+        3)  
+            if test -f vuelo.intento -a -x vuelo.intento; then 
+                get_passengers
+                ./vuelo.intento $assistants           
+            else
+                echo "Fallo al intentar ejecutar el vuelo, comprueba y vuelve a intentar"
+            fi
+            ;;
+        4)
+            echo "Ya has tenido suficientes vuelos, saliendo"
+            exit 0
+            ;;
+        *) 
+            echo "Esta opcion no existe, vuelve a intentar con una opcion valida"
+            ;;
+        esac
     done
-}
+
+
