3fb83ef9068e6c8a8b3e0fc2f4cd58948467fbdd
Raul <rfernr08@estudiantes.unileon.es>
2023-11-16 12:51:19 +0100

    cambios de signal a sigaction

--- PracticaIntermedia/PracticaIntermedia.c
+++ PracticaIntermedia/PracticaIntermedia.c
@@ -3,23 +3,25 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
 #include <time.h>
 
-void tecnico(int i);
-void encargado(int i);
-void asistente(int i);
+void tecnicoHandler(int i);
+void encargadoHandler(int i);
+void asistenteHandler(int i);
 
 int main(int argc, char*argv[]){ //coordinador es  padre
+    struct sigaction ss;
     srand (time(NULL));
     pid_t tecnico, encargado, a;
     tecnico = fork();
     if(tecnico == 0){
+        ss.sa_handler = &tecnicoHandler;
         pause();
     }
     encargado = fork(); 
     if(encargado == 0){
+        ss.sa_handler = &encargadoHandler;  
         pause();
     }
     int asistentes[argc];
@@ -34,21 +36,21 @@ int main(int argc, char*argv[]){ //coordinador es  padre
     }
 
     sleep(2);
-    int vuelo = signal(SIGUSR1, tecnico);
+    int vuelo = sigaction(SIGUSR1, &ss, NULL);
     pause();
     if(vuelo == 0){
         kill(tecnico, SIGTERM);
-        kill(encargado, SIGTERM)
+        kill(encargado, SIGTERM);
         for(int i = 0; i < asistentes.length(); i++){
             kill(asistente[i], SIGTERM);
         }
     }
 
     int pasajeros = 0;
-    int overbooking = signal(SIGUSR1, encargado);
+    int overbooking = sigaction(SIGUSR1, &ss, NULL);
     pause();
     for(int i = 0; i < asistentes.length(); i++){
-        pasajeros =  pasajeros + signal(SIGUSR2, asistentes[i]);
+        pasajeros =  pasajeros + sigaction(SIGUSR2, &ss, NULL);//signal(SIGUSR2, asistentes[i]);
     }
     if(overbooking == 1){
         pasajeros = pasajeros -10;
@@ -56,19 +58,19 @@ int main(int argc, char*argv[]){ //coordinador es  padre
     printf(pasajeros);
 }
 
-    void tecnico(int i){
+    void tecnicoHandler(int i){
         printf("Tecnico contactado");
         sleep(rand() %4 + 3);
         exit(rand() %2 + 0);
     }
 
-    void encargado(int i){
+    void encargadoHandler(int i){
         printf("Encargado contactado");
         sleep(2);
         exit(rand() %2 + 0);
     }
 
-    void asistente(int i){
+    void asistenteHandler(int i){
         printf("Asistente" + getpid() + "contactado");
         sleep(rand() %4 + 3);
         exit(rand() %11 + 20);
